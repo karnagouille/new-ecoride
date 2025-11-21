30f5a061e4eae99288f84b3cb3ac7cb6c29daaa7
karnage <gonzalesalexis999@gmail.com>
2025-11-21 13:46:22 +0100

    RAS

---
+++ src/Controller/EmployeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+final class EmployeController extends AbstractController
+{
+
+    #[IsGranted('ROLE_EMPLOYE')]
+    #[Route('/employe', name: 'employe')]
+    public function index(): Response
+    {
+        return $this->render('employe/index.html.twig', [
+            'controller_name' => 'EmployeController',
+        ]);
+    }
+}
