e0621145af478e48fff65146eccf874e3940c0b5
karnage <gonzalesalexis999@gmail.com>
2026-04-03 12:17:44 +0200

    css

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,16 +3,12 @@
 namespace App\Form;
 
 use App\Entity\User;
-
+use Symfony\Component\Form\Extension\Core\Type\TextType; 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Validator\Constraints\Image;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
@@ -24,7 +20,7 @@ class RegistrationFormType extends AbstractType
             ->add('email')
             ->add('pseudo')
             ->add('name')
-            ->add('lastname')
+            ->add('lastname')           
             
             ->add('plainPassword', RepeatedType::class, [
                 'type'   => PasswordType::class,            
@@ -41,11 +37,11 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
                     'first_options'=>[
-                        'label'=> 'Votre mot de passe :',
+                        'label'=> 'Votre mot de passe : ',
 
                     ],
                     'second_options'=>[
-                        'label'=> 'confirmez votre mot de passe :',
+                        'label'=> 'confirmez votre mot de passe : ',
                         
                     ]
             ])
